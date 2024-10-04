57f66fbfe2456eea8f7e8c1b30e3b2c9cdf6d871
christyTam <christyjames938@yahoo.com>
2024-10-04 05:14:51 +0000

    my project

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Pascal Triangle Generator
+"""
+
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to the n-th row.
+
+    Args:
+        n (int): The number of rows to generate.
+
+    Returns:
+        list: A list of lists representing Pascal's Triangle.
+              Returns an empty list if n <= 0.
+    """
+    result = []
+    if n <= 0:
+        return result
+    result = [[1]]
+    for current_row in range(1, n):
+        new_row = [1]
+        for current_column in range(len(result[current_row - 1]) - 1):
+            previous_row = result[current_row - 1]
+            new_row.append(previous_row[current_column] + previous_row[current_column + 1])
+        new_row.append(1)
+        result.append(new_row)
+    return result
